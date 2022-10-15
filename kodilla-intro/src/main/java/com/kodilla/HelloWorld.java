d42bdc1f3a784154ad087e545aa875a516ccddf9
Rafał Zym <rzym34@gmail.com>
2022-10-15 21:24:58 +0200

    0.7 Typy Danych - Hello World

---
+++ kodilla-intro/src/main/java/com/kodilla/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.kodilla;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Rafał");
+        System.out.println(23);
+        System.out.println('Z');
+
+    }
+}
