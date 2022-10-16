1a48dba2dde80e73d59cdfb034eeb4be558717f1
Rafał Zym <rzym34@gmail.com>
2022-10-16 10:47:47 +0200

    0.8 Zmienne - deklarowanie i wywoływanie zmiennych

---
+++ kodilla-intro/src/main/java/com/kodilla/MainExtended.java
@@ -0,0 +1,19 @@
+package com.kodilla;
+
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Rafał";
+        String mySurname = "Zym";
+        int myAge = 23;
+        double myHeight = 1.80;
+        char firstLetterMyName = 'R';
+        boolean iAmKodillaTester = true;
+        System.out.println(myName);
+        System.out.println(mySurname);
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println(firstLetterMyName);
+        System.out.println(iAmKodillaTester);
+
+    }
+}
