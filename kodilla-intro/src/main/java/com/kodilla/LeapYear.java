4af37565b963a6c2a6f4c83021673222120a4364
Rafał Zym <rzym34@gmail.com>
2022-11-13 20:21:19 +0100

    0.11 Zadanie - rok przestępny - poprawa

--- kodilla-intro/src/main/java/com/kodilla/LeapYear.java
+++ kodilla-intro/src/main/java/com/kodilla/LeapYear.java
@@ -1,16 +1,23 @@
 package com.kodilla;
 
 public class LeapYear {
-    public static void main(String[] args) {
-        int year = 2022;
 
+    public static boolean isLeapYear(int year) {
         if (year % 4 == 0) {
-            System.out.println("rok przestępny");
-        } else {
-            System.out.println("rok nieprzestępny");
+            if (year % 100 == 0){
+                if (year % 400 == 0) {
+                    return true;
+                }
+                return false;
+            }
+            return true;
         }
+        return false;
+    }
 
-
+    public static void main(String[] args) {
+        boolean result = isLeapYear(2020);
+        System.out.println(result);
     }
 
 }
