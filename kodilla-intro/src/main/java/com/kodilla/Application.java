2c75e6491a100cf8ef72366314063880c60e3c03
Rafał Zym <rzym34@gmail.com>
2022-11-20 16:12:56 +0100

    Ćwiczenie 2.6 Refaktoryzacja kodu - poprawa

--- kodilla-intro/src/main/java/com/kodilla/Application.java
+++ kodilla-intro/src/main/java/com/kodilla/Application.java
@@ -12,10 +12,12 @@ public class Application {
     }
 
     public static void main(String[] args) {
-
         Application adam = new Application("Adam", 40.5, 178);
+        adam.getInfoBasedOnAgeAngHeight();
+    }
 
-        if (adam.age > 30 && adam.height > 160) {
+    public void getInfoBasedOnAgeAngHeight() {
+        if (age > 30 && height > 160) {
             System.out.println("User is older than 30 and taller than 160cm");
         } else {
             System.out.println("User is 30 (or younger) or 160cm (or shorter)");
