23f3e820aa5494bb5ff139f1375a11e7f1435691
Rafał Zym <rzym34@gmail.com>
2023-02-06 17:24:15 +0100

    Exercise 13.4 Poprawka

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -1,6 +1,5 @@
 package com.kodilla.selenium.allegro;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,8 +12,8 @@ public class AllegroTestingApp {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.allegro.pl");
 
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
+        WebElement cookiesCombo = driver.findElement(By.xpath("//*[@class=\"mgn2_14 mp0t_0a m9qz_yp mp7g_oh mse2_40 mqu1_40 mtsp_ib mli8_k4 mp4t_0 munh_0 m911_5r mefy_5r mnyp_5r mdwl_5r msbw_2 mldj_2 mtag_2 mm2b_2 mqvr_2 msa3_z4 mqen_m6 meqh_en m0qj_5r mh36_16 mvrt_16 mg9e_0 mj7a_0 mjir_sv m2ha_2 m8qd_qh mjt1_n2 b1vf8 mgmw_9u msts_enp mrmn_qo mrhf_u8 m31c_kb m0ux_fp bnpxh mjru_k4 _158e2_4-oWM m3h2_0 m3h2_16_s mryx_24 mryx_0_s mryx_24_x m7er_0k\"]"));
+        cookiesCombo.click();
 
         WebElement categoryCombo = driver.findElement(By.xpath("//*[@class=\"mr3m_1 m7er_k4 mj6k_n7 sy40i mgn2_14 mp0t_0a mqu1_21 mgmw_wo mli8_k4\"]"));
         Select categorySelect = new Select(categoryCombo);
